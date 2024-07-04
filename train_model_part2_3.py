186542d65dbf104ddd6f1aef10d84211caae7678
sonnt-dna <110382822+sonnt-dna@users.noreply.github.com>
2024-07-04 13:47:11 +0700

    Delete train_model_part2_3.py

--- train_model_part2_3.py
+++
@@ -1,6 +0,0 @@
-# đênh gia mộnh tượng kế nhân
-gy_pred = best_model.predict(XTest)
-mse = mean_squared_error(yTest, y_pred)
-r2 = r2score(yTest, y_pred)
-print('Mean Squared Error: ${MSE}')
-print('R^2 Score: ${R2}')
